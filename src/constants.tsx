3dd777db59faffa4f9b6cb83e98ddbefaa970d4e
Muhammad Ahmed <ahmed.azhar2299@gmail.com>
2024-02-05 19:17:06 +0500

    Feat | Experience section added

--- src/constants.tsx
+++ src/constants.tsx
@@ -5,3 +5,29 @@ export enum PROFILE {
 export enum EDUCATION {
   UNIVERSITY_IMAGE = "https://upload.wikimedia.org/wikipedia/en/e/e4/National_University_of_Computer_and_Emerging_Sciences_logo.png",
 }
+
+export enum COMPANY {
+  XIQ_LOGO = "https://media.xiq.io/wepapp/wp-content/uploads/2020/05/19160335/xiQ-logo.png",
+  TINTASH_LOGO = "https://images.softwaresuggest.com/company_logo/1565605957011_b_124.png",
+}
+
+export const EXPERIENCE_DATA = [
+  {
+    designation: "Associate Full Stack",
+    company_name: "xiQ,Inc.",
+    logo: COMPANY.XIQ_LOGO as string,
+    duration: "JULY 2023 - Current",
+    description: [
+      "WORKED IN GENERATIVE AI",
+      "WORKED IN GENERATIVE AI",
+      "WORKED IN GENERATIVE AI",
+    ],
+  },
+  {
+    designation: "MERN STACK INTERN",
+    company_name: "Tintash",
+    duration: "JULY 2023 - Current",
+    logo: COMPANY.TINTASH_LOGO as string,
+    description: ["WORKED IN GENERATIVE AI"],
+  },
+];
